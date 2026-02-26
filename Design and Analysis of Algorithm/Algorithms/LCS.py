7811f5f6de18fc3288e101280d6105721b9b5594
jeetsaha1 <jeet2022saha@gmail.com>
2026-02-27 02:25:41 +0530

    Corrected version

--- Design and Analysis of Algorithm/Algorithms/LCS.py
+++ Design and Analysis of Algorithm/Algorithms/LCS.py
@@ -13,16 +13,20 @@ def LCS_string(A,B,i,j):
 
 
 # Dynamic Programming approach  (Time Complexity --> O(m*n))
-from functools import lru_cache
+def LCS(A, B):
+    m = len(A)
+    n = len(B)
 
-@lru_cache(None)
-def LCS_string(A,B,i, j):
-    if i == 0 or j == 0:
-        return 0
-    if A[i-1] == B[j-1]:
-        return 1 + LCS_string(A,B,i-1, j-1)
-    return max(LCS_string(A,B,i-1, j), LCS_string(A,B,i, j-1))
+    dp = [[0]*(n+1) for _ in range(m+1)]
 
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if A[i-1] == B[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return dp[m][n], dp
 
 A = "ABCBDAB"
 B = "BDCABA"
